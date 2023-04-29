7066c63d93b78589f21517108abfe31ed677fe96
Ayush Pawar <i.yush.004@gmail.com>
2023-04-29 18:11:25 +0530

    ui fixes

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,5 @@
-int daysBetween(DateTime from, DateTime to) {
-  from = DateTime(from.year, from.month, from.day);
+int daysBetween(DateTime froms, DateTime to) {
+  froms = DateTime(froms.year, froms.month, froms.day);
   to = DateTime(to.year, to.month, to.day);
-  return (to.difference(from).inHours / 24).round();
+  return (to.difference(froms).inHours / 24).round();
 }
-
-
